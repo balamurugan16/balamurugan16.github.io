67c2157ce43fcdb4fbf8f29c5bdc2c0e66b1c55e
balamurugan.16 <balamurugan.47@yahoo.com>
2020-05-29 13:18:21 +0530

    So the responsiveness of the website is being tested and the script files are ready as of now. JS touch and click events are added. Hopefully in the next commit, the responsive window will be ready

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-balamurugan16.github.io
-hello world
+#balamurugan16.github.io
+This is a fun website where a drumkit is providid and the user can simply spend his/her time in playing the drums.
+🤣😂
+Hope you enjoy it.
--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 window.addEventListener('keydown',playSound);
+let keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key=>key.addEventListener('click',playsound2))
+keys.forEach(key=>key.addEventListener('touchstart',playsound2))
+function playsound2(e){
+     let value = this.getAttribute('data-key'); 
+     let audio = document.querySelector(`audio[data-key="${value}"]`)    
+     audio.currentTime = 0;
+     this.classList.add('playing');
+     audio.play();
+}
 function playSound(e){
      let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);    
      let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -7,7 +17,7 @@ function playSound(e){
      key.classList.add('playing');
      audio.play();
 }
-let keys = Array.from(document.querySelectorAll('.key'));
+
 keys.forEach((key)=>key.addEventListener('transitionend',removeTransition));
 
 function removeTransition(e){
